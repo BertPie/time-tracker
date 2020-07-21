df6e8f5c07c4557af2c6c93addb3ca847988dce8
Bert Pie <BertPieGIT@gmail.com>
2020-07-21 13:03:18 +0200

    create stub Spring Boot application, create smoke test

---
+++ src/test/java/pie/bert/tracker/TimeTrackerApplicationTest.java
@@ -0,0 +1,13 @@
+package pie.bert.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTrackerApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
